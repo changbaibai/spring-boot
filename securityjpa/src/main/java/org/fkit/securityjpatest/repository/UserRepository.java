4d52fb2df29b67798648663b639b1888a8085ed6
changbaibai <1849300964@qq.com>
2019-12-10 17:01:35 +0800

    基于jpa的security安全控制登录注册

---
+++ securityjpa/src/main/java/org/fkit/securityjpatest/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.fkit.securityjpatest.repository;
+
+
+import org.fkit.securityjpatest.pojo.FKUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<FKUser, Long>{
+
+	// 根据登录名查询出用户
+	FKUser findByLoginName(String loginName);
+
+
+}
